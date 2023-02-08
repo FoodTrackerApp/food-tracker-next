9933307e352fe510f6e319fd52c466db9f05eb71
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2023-02-08 19:02:25 +0100

    Fixed send not working

--- pages/api/replace.js
+++ pages/api/replace.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
 
    // Take the already synced data from the request body
    let data = JSON.parse(req.body);
+   console.log("new data", data);
 
    // change id to _id
    data.forEach(item => {
@@ -17,20 +18,6 @@ export default async function handler(req, res) {
 
    // Save the new items
    await saveItem(data);
-
-   /*
-   data.forEach(async item => {
-      // if item is already local, overwrite the one with older datemodified
-      const isLocal = localItems.findIndex(localItem => localItem._id === item._id);
-      if(isLocal && isLocal.datemodified > item.datemodified) {
-         console.log(isLocal.name, 'is already local, but has newer datemodified');
-         await saveItem(isLocal);
-      } else {
-         // add new item
-         console.log('adding new item', item.name);
-         await saveItem(item);
-      }
-   })*/
 
    res.status(200).json(data);
 }
